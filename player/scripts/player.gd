42a66cfc0a9e43b58fb9837306f9ce1e284631ff
Akbar Maliki <akbarmaliki109@gmail.com>
2024-04-05 14:44:56 +0700

    add catapult mechanics

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -8,3 +8,11 @@ func start():
 
 func _physics_process(delta):
 	move_and_slide()
+
+
+func _on_catapult_vector_created(vector):
+	var stateMachine = $StateMachine
+	var currentState:State = stateMachine.current_state
+	velocity += vector * 10
+	move_and_slide()
+	currentState.state_transition_signal.emit(currentState, 'PlayerFall')

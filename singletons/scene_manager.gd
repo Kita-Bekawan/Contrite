88d1db86b07bd55b4a7fdee9890b0211981754ae
figoathallah <figoirawan02@gmail.com>
2024-04-24 18:12:02 +0700

    add level 4 layout, obstacles and boss

--- singletons/scene_manager.gd
+++ singletons/scene_manager.gd
@@ -2,7 +2,8 @@ extends Node
 
 var scenes : Dictionary = { "Level4": "res://level/scenes/level_4.tscn",
 							"Level4A": "res://level/scenes/level_4a.tscn",
-							"Level4B": "res://level/scenes/level_4b.tscn"}
+							"Level4B": "res://level/scenes/level_4b.tscn",
+							"Level4C": "res://level/scenes/level_4c_boss.tscn"}
 							
 func transition_to_scene(level: String):
 	var scene_path: String = scenes.get(level)
